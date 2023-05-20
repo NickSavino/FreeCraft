cc37d0a84e66ec398b70b8e6caff175648c90be5
desmond.obrien <des@Dess-Air.hitronhub.home>
2023-05-20 02:15:38 -0600

    multiple selection working

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -12,7 +12,8 @@ public class UI : MonoBehaviour
     Vector3 mouseClickPosition;
     Vector3 mouseDragPosition;
     GameObject selectionBox;
-
+    bool mouseHeld = false;
+    RectTransform trans;
     
 
 
@@ -24,7 +25,7 @@ public class UI : MonoBehaviour
         this.mouseSelection = new Rect();
         this.mouseClickPosition = new Vector3(0, 0, 0);
         this.mouseDragPosition = new Vector3(0, 0, 0);
-     
+        trans = gameObject.AddComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -45,8 +46,9 @@ public class UI : MonoBehaviour
         {
             // if it was, set the top-left corner of the box to this position
             this.mouseClickPosition = Input.mousePosition;
-            this.mouseSelection.min = this.mouseDragPosition;
-            
+            this.mouseClickPosition.z = 10;
+            this.mouseSelection.min = Camera.main.ScreenToWorldPoint(this.mouseClickPosition);
+
         }
 
         // check to see if the mouse button was held at the beginning of this frame
@@ -55,20 +57,49 @@ public class UI : MonoBehaviour
             // if it was, update
             this.mouseDragPosition = Input.mousePosition;
             this.mouseDragPosition.z = 10;
-            this.mouseSelection.max = this.mouseDragPosition;
+            this.mouseSelection.max = Camera.main.ScreenToWorldPoint(this.mouseDragPosition);
 
-            Debug.Log(this.mouseClickPosition);
-            Debug.Log(this.mouseDragPosition);
 
+            mouseHeld = true;
+        }
+
+        if (!Input.GetMouseButton(0) && mouseHeld)
+        {
+            mouseHeld = false;
+            this.selectedUnits.Clear();
+            Vector3 corner1 = this.mouseSelection.min;
+            Vector3 corner2 = new Vector3(this.mouseSelection.max.x, this.mouseSelection.min.y, 10);
+            Vector3 corner3 = this.mouseSelection.max;
+            Vector3 corner0 = new Vector3(this.mouseSelection.min.x, this.mouseSelection.max.y, 10);
+
+
+            Vector3[] corners = new Vector3[] { corner0, corner1, corner2, corner3 };
+
+            //trans.GetWorldCorners(corners);
+
+            foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+            {   // if within x range
+                bool xCheck = Camera.main.ScreenToWorldPoint(unit.transform.position).x >= Camera.main.ScreenToWorldPoint(corners[0]).x && Camera.main.ScreenToWorldPoint(unit.transform.position).x <= Camera.main.ScreenToWorldPoint(corners[2]).x;
+                bool yCheck = Camera.main.ScreenToWorldPoint(unit.transform.position).y >= Camera.main.ScreenToWorldPoint(corners[0]).y && Camera.main.ScreenToWorldPoint(unit.transform.position).y <= Camera.main.ScreenToWorldPoint(corners[1]).y;
+
+                if (xCheck && yCheck)
+                {
+                    this.selectedUnits.Add(unit);
+                    Debug.Log("Here");
+                }
             }
+
+
+
+
+            foreach (GameObject unit in this.selectedUnits)
+            {
+                Debug.Log(unit.name);
  
-        
+            }
 
-
-
-
+        }
     }
-
 
     /**
      * 
--- Assets/UnitInfantry.cs
+++ Assets/UnitInfantry.cs
@@ -50,5 +50,6 @@ public class UnitInfantry : MonoBehaviour, Unit
 
     void attackUnit() {
         //handles unit performing attacks
+        
     }
 }
